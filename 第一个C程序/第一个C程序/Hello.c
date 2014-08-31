50e7092462cf84b6caa14301def2c4ffbec143f5
panyi <525647740@qq.com>
2014-08-31 09:15:05 +0800

    第一个c程序

---
+++ 第一个C程序/第一个C程序/Hello.c
@@ -0,0 +1,15 @@
+//
+//  Hello.c
+//  第一个C程序
+//
+//  Created by  潘易  on 14-8-31.
+//  Copyright (c) 2014年 xinlan. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World!\n你好 世界\n");
+    return 0;
+}
